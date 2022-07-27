a4efb711886e3a8b21a4cdce2cbf2454e1968005
Carson <liucarson@yahoo.com>
2022-07-26 18:57:56 -0700

    added schemas/tables for Postgresql and mongodb

--- models/qaModel.js
+++ models/qaModel.js
@@ -1,7 +1,33 @@
 const { Pool } = require('pg')
+const mongoose = require('mongoose')
+
+const questionsSchema = mongoose.Schema({
+  product_id: { type: Number, required: true },
+  body: { type: String, required: true },
+  date_written: Number,
+  asker_name: { type: String, required: true },
+  asker_email: { type: String, required: true },
+  reported: { type: Boolean, default: false },
+  helpful: { type: Number, default: 0 },
+})
+
+const answersSchema = mongoose.Schema({
+  question_id: { type: Number, required: true },
+  body: { type: String, required: true },
+  date_written: Number,
+  answerer_name: { type: String, required: true },
+  answerer_email: { type: String, required: true },
+  reported: { type: Boolean, default: false },
+  helpful: { type: Number, default: 0 },
+  photos: [String]
+})
 
 const pool = new Pool({
-
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  port: process.env.PGPORT,
+  password: process.env.PGPASS,
+  database: process.env.PGDATABASE,
 })
 
 const getQuestions = (id, cb) => {
@@ -9,6 +35,14 @@ const getQuestions = (id, cb) => {
 }
 
 const getAnswers = (id, cb) => {
+
+}
+
+const postQuestion = (id, cb) => {
+
+}
+
+const postAnswer = (id, cb) => {
 
 }
 
@@ -28,9 +62,14 @@ const answerHelpful = (id, cb) => {
 
 }
 
+
+
+
 module.exports = {
   getQuestions,
   getAnswers,
+  postQuestion,
+  postAnswer,
   reportQuestion,
   reportAnswer,
   questionHelpful,
---
+++ schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS qa;
+CREATE DATABASE qa;
+\c qa;
+
+DROP TABLE IF EXISTS questions, answers, photos CASCADE;
+
+CREATE TABLE IF NOT EXISTS questions(
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  product_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+  date_written BIGINT,
+  asker_name VARCHAR(50) NOT NULL,
+  asker_email VARCHAR(50) NOT NULL,
+  reported BOOLEAN,
+  helpful INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS answers(
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  question_id INTEGER REFERENCES questions(id),
+  body TEXT NOT NULL,
+  date_written BIGINT,
+  answerer_name VARCHAR(50) NOT NULL,
+  answerer_email VARCHAR(50) NOT NULL,
+  reported BOOLEAN,
+  helpful INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS photos(
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  answer_id INTEGER REFERENCES answers(id),
+  url TEXT
+);
+
+\COPY questions FROM 'csv/questions.csv' delimiter ',' CSV HEADER;
+\COPY answers FROM 'csv/answers.csv' delimiter ',' CSV HEADER;
+\COPY photos FROM 'csv/answers_photos.csv' delimiter ',' CSV HEADER;
